7a29dc1b1216534e4e5683b42c610b984bfafaab
maxdouglas <bmaxdouglas@gmail.com>
2024-05-02 15:37:13 -0300

    commit react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import People from './assets/people.svg'
+
+import {
+  H1,
+  Container,
+  Image,
+  ContainerItens,
+  Input,
+  InputLabel,
+  Button,
+} from "./styles";
 
 const App = () => {
   return (
-    <div>
-      {" "}
-      <h1>Hello Word</h1>
-    </div>
+    <Container>
+      <Image alt="logo-Img " src={People}/>
+      <ContainerItens/>
+      <H1>Hello!</H1>
+      <InputLabel>Nome</InputLabel>
+      <Input placeholder="Nome"/>
+      <InputLabel>Idade</InputLabel>
+      <Input placeholder="Idade"/>
+      <Button>Cadastrar</Button>
+    </Container>
   );
 };
 
---
+++ src/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import background from "./assets/backgroundIni.svg"
+
+
+export const Container = styled.div `
+ background: url("${background}");
+ background-size: cover;
+
+`
+export const Image = styled.img `
+`
+export const ContainerItens = styled.div `
+
+`
+export const H1 = styled.h1 `
+
+`
+
+export const InputLabel = styled.p `
+
+`
+export const Input = styled.input `
+
+`
+export const Button = styled.button `
+
+`
